d25b6804d8398a61a78c4ad5096db73afc93db00
SwingIT <swingit.affiliate@gmail.com>
2025-04-08 03:20:17 +0200

    Yet more stuff..

---
+++ components/supabase-provider.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import type React from "react"
+
+import { createContext, useContext, useState, useEffect } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import type { SupabaseClient } from "@supabase/supabase-js"
+
+type SupabaseContext = {
+  supabase: SupabaseClient
+}
+
+const Context = createContext<SupabaseContext | undefined>(undefined)
+
+export function SupabaseProvider({ children }: { children: React.ReactNode }) {
+  const [supabase] = useState(() => createClientComponentClient())
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {})
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [supabase])
+
+  return <Context.Provider value={{ supabase }}>{children}</Context.Provider>
+}
+
+export const useSupabase = () => {
+  const context = useContext(Context)
+  if (context === undefined) {
+    throw new Error("useSupabase must be used within a SupabaseProvider")
+  }
+  return context
+}
